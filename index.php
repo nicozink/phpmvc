9d5da432881c7ab46c6d958658a182dc01b13b02
Nico Zink <nico@nfzink.com>
2013-12-30 23:29:44 +0200

    Development #6: Connected to database, and read some test data.

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
- echo "Hello, world!";
+  echo "Hello, world! <br>";
+
+  // Create connection
+  $con = mysqli_connect("localhost", "dbt_user", "dbt_password", "dbt_master");
+
+  if (mysqli_connect_errno())
+  {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+  $result = mysqli_query($con, "SELECT * FROM dbt_account");
+
+  while($row = mysqli_fetch_array($result))
+  {
+    echo $row['data'];
+    echo "<br>";
+  }
+  
+  mysqli_close($con);
 ?>
