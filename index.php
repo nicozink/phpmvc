e5c99f097e3e32fdc14e3d23495c855b1ea05a71
Nico Zink <nico@nfzink.com>
2013-12-31 15:31:58 +0200

    Development #3: Added parsing of URI.

--- index.php
+++ index.php
@@ -1,12 +1,42 @@
 <?php
-  echo "Hello, world! <br>";
+  echo "Server: ".$_SERVER["REQUEST_URI"]."<br>";
+  echo "Script: ".$_SERVER["SCRIPT_NAME"]."<br>";
+  echo "<br>";
+  
+  $requestURI = explode("/", $_SERVER["REQUEST_URI"]);
+  $scriptName = explode("/", $_SERVER["SCRIPT_NAME"]);
+
+  for($i = 0; $i < sizeof($requestURI); $i++)
+  {
+    if ($requestURI[$i] == null || $requestURI[$i] == "")
+    {
+      unset($requestURI[$i]);
+    }
+  }
+  
+  $command = array_values($requestURI);
+
+  switch ($command[0])
+  {
+    case "commandOne" :
+    echo "You entered command: ".$command[0]."<br>";
+    break;
+
+    case "commandTwo" :
+    echo "You entered command: ".$command[0]."<br>";
+    break;
+
+    default:
+    echo "That command does not exist: ".print_r($command)."<br>";
+    break;
+  }
 
   // Create connection
   $con = mysqli_connect("localhost", "dbt_user", "dbt_password", "dbt_master");
 
   if (mysqli_connect_errno())
   {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error()."<br>";
   }
   
   $result = mysqli_query($con, "SELECT * FROM dbt_account");
